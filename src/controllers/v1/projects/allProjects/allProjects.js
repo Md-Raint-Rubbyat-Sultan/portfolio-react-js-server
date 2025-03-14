465d6aa8719de6f4529d446ebba7b860cfa3b770
Md-Raint-Rubbyat-Sultan <mdranitrubbyatsultan@gmail.com>
2025-03-14 17:42:47 +0600

    bug fixed

--- src/controllers/v1/projects/allProjects/allProjects.js
+++ src/controllers/v1/projects/allProjects/allProjects.js
@@ -1,10 +1,19 @@
 import projects from "../../../../models/projects/projects.js";
 
 const allProjects = async (req, res) => {
-  const countProject = await projects.estimatedDocumentCount({});
-  const lim = req?.query?.lim || countProject;
-  const result = await projects.find({}).limit(lim);
-  res.status(200).send({ countProject, data: result });
+  try {
+    const countProject = await projects.estimatedDocumentCount({});
+
+    const lim =
+      (typeof parseInt(req?.query?.lim) === "number" &&
+        parseInt(req?.query?.lim)) ||
+      countProject;
+
+    const result = await projects.find({}).limit(lim);
+    res.status(200).send({ countProject, data: result });
+  } catch (err) {
+    res.send(err.message);
+  }
 };
 
 export default allProjects;
