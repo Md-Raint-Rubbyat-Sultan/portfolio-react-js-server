43f152a66bc794bd997c8cd3c418c1d2a4f20fb4
Md-Raint-Rubbyat-Sultan <mdranitrubbyatsultan@gmail.com>
2025-03-30 15:28:28 +0600

    data deletion is not working

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,6 @@ import test from "./test/tests.js";
 import projects from "./projects/projects.js";
 import adminData from "./adminData/adminData.js";
 import auth from "./auth/auth.route.js";
-import facebookDeletion from "../utils/facebookDeletion.js";
 
 const router = express.Router();
 
@@ -19,6 +18,4 @@ router.use("/adminData", adminData);
 // auth routes
 router.use("/auth", auth);
 
-// facebook app for auth connection
-router.use("/facebook-deletion", facebookDeletion);
 export default router;
